28bb7baa358ba0a0a8cbf493a01181239d17727d
interluebke <111441242+interluebke@users.noreply.github.com>
2026-01-20 10:37:38 +0100

    Refine calculate function§

--- src/script.js
+++ src/script.js
@@ -1,16 +1,19 @@
 const buttons = document.querySelectorAll("button")
 
 function calculator() {
-    let num1;
+    let num1 = 1;
     let operator;
-    let num2;
+    let num2 = 2;
 
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
-            alert(button.id);
+            alert(typeof(button.id));
+            alert(calculate(num1, button.id, num2));
         });
     });
 };
+
+
 
 function appendNumbers(id) {
     if (operator === undefined) {
@@ -21,16 +24,21 @@ function appendNumbers(id) {
 };
 
 function calculate(n1, op, n2) {
-    switch(op) {
-        case op === "add":
-            return n1 + n2;
-        case op === "subtract":
-            return n1 - n2;
-        case op === "multiply":
-            return n1 * n2;
-        case op === "divide":
-            return n1 / n2;
-    };
+    if (n1 === undefined || n2 == undefined) {
+        alert("Invalid Operation!");
+        calculator();
+    } else {
+        switch(op) {
+            case "add":
+                return n1 + n2;
+            case "subtract":
+                return n1 - n2;
+            case "multiply":
+                return n1 * n2;
+            case "divide":
+                return n1 / n2;
+        };
+    };  
 };
 
 calculator();
